fd48bc1ae60125d9ba29f5be4675864438544fe3
Naruepanat <cstaytay@hotmail.com>
2018-03-30 15:10:06 +0700

    Setup app for huroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,21 @@
 var mongoose = require('mongoose');
 
+
+if(process.env.PORT){
+    connectPath = "mongodb://<dbuser>:<dbpassword>@ds229549.mlab.com:29549/todoapp";
+    options= {
+        auth: {
+            user: 'sys',
+            password: 'admin'
+        }
+    }
+   }else{
+    connectPath = "mongodb://localhost:27017/TodoApp";
+    options = {}
+   }
+
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(connectPath,options);
 
 module.exports={mongoose};
